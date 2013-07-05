3728f79098d9a2439d8e23ef3f21cf832c9100d9
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-07-05 11:48:09 -0700

    Stereotypical sample Android app

---
+++ MyApplication/src/instrumentTest/java/com.example.myapplication/FooTest.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.test.ActivityTestCase;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by kohsuke on 7/5/13.
+ */
+public class FooTest extends ActivityTestCase {
+    public void test1() {}
+    public void test2() {}
+    public void test3() {}
+}
